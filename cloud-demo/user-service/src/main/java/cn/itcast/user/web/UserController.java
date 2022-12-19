1d9fa39aa18fed6a9158233c33e8981390c9d801
zhangjiaxun <20302010032@fudan.edu.cn>
2022-12-19 18:04:39 +0800

    可以注册

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -2,6 +2,7 @@ package cn.itcast.user.web;
 
 
 import cn.itcast.feign.common.Result;
+import cn.itcast.user.common.AddUserData;
 import cn.itcast.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @PostMapping("/addUser")
+    public boolean addUser(@RequestBody AddUserData user){
+        try{
+            userService.addUser(user.ToUser());
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     @PostMapping("/info")
     public Result info(HttpServletRequest request) {
