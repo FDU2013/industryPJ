6d765f8dafa0218d6e4eaa7e11f5d52ffa3cc9c0
zhangjiaxun <20302010032@fudan.edu.cn>
2022-12-20 16:06:52 +0800

    完善文档

---
+++ cloud-demo/shop-service/src/main/java/cn/itcast/order/web/CommentController.java
@@ -0,0 +1,11 @@
+package cn.itcast.order.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shop")
+public class CommentController {
+
+}
